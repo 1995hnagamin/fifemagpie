5161b46208241458f630b6e6aa43566063709a6a
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2024-09-19 15:39:37 +0900

    Add example.py

---
+++ example.py
@@ -0,0 +1,12 @@
+from scipy.sparse import lil_matrix
+import melampyfem as fem
+
+ndiv = 10
+Th = fem.mesh2d.make_square_mesh(ndiv, ndiv)
+print(Th)
+
+def make_coeff_matrix(th):
+    n = fem.mesh2d.nvtx(th)
+    lil = lil_matrix((n, n), dtype=np)
+    for vise in th.vises:
+        qlat = fem.mesh2d.vise_to_qlat(vise)        
