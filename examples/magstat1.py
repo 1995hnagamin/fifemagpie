4aa9f0655e7cbef6f99ae2b953df977fd8d71e30
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2024-09-20 22:49:07 +0900

    Update examples/magstat1.py

--- examples/magstat1.py
+++ examples/magstat1.py
@@ -1,12 +1,47 @@
-from scipy.sparse import lil_matrix
+import numpy as np
+from scipy.sparse import dok_matrix
 import fifemagpie as fem
 
 ndiv = 10
 Th = fem.mesh2d.make_square_mesh(ndiv, ndiv)
-print(Th)
+
+
+def N(xi, et):
+    return np.array(
+        [
+            (1 - xi) * (1 - et) / 4,
+            (1 + xi) * (1 - et) / 4,
+            (1 + xi) * (1 + et) / 4,
+            (1 - xi) * (1 + et) / 4,
+        ]
+    )
+
+
+def dNdxiet(xi, et):
+    return np.array(
+        [
+            [-(1 - et) / 4, +(1 - et) / 4, +(1 + et) / 4, -(1 + et) / 4],
+            [-(1 - xi) / 4, -(1 + xi) / 4, +(1 + xi) / 4, +(1 - xi) / 4],
+        ]
+    )
+
+
+# [dx/dxi dx/det; dy/dxt dy/det]
+def dxydxiet(qlat, xi, et):
+    dx = dNdxiet(xi, et).dot(qlat.xs)
+    dy = dNdxiet(xi, et).dot(qlat.ys)
+    return np.array([dx, dy])
+
 
 def make_coeff_matrix(th):
     n = fem.mesh2d.nvtx(th)
-    lil = lil_matrix((n, n), dtype=np)
+    dok = dok_matrix((n, n), dtype=np.float64)
     for vise in th.vises:
-        qlat = fem.mesh2d.vise_to_qlat(vise)        
+        qlat = fem.mesh2d.vise_to_qlat(th, vise)
+        for i in range(4):
+            for j in range(4):
+                dok[vise[i], vise[j]] += 1
+    return dok.tocsr()
+
+
+# print(make_coeff_matrix(Th))
